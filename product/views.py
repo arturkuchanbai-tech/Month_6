ce9c0e125cd0a816c8ac13f2f93f65087836447b
Artur <arturkuchanbai@gmail.com>
2026-03-04 16:00:29 +0600

    h/w_3ready

--- product/views.py
+++ product/views.py
@@ -9,41 +9,88 @@ from .serializers import (
     ReviewDetailSerializer, ReviewListSerializer
 )
 
-
+@api_view(['GET'])
+def category_list_view(request):
+    categories = Category.objects.all()
+    serializer = CategoryListSerializer(categories, many=True)
+    return Response(serializer.data)
 @api_view(['GET'])
 def category_detail_view(request, id):
     category = get_object_or_404(Category, id=id)
     serializer = CategoryDetailSerializer(category)
     return Response(serializer.data)
+@api_view(['POST'])
+def category_create_view(request):
+    serializer = CategoryDetailSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+@api_view(['PUT', 'PATCH', 'DELETE'])
+def category_modify_view(request, id):
+    category = get_object_or_404(Category, id=id)
 
-@api_view(['GET'])
-def category_list_view(request):
-    categories = Category.objects.all()
-    serializer = CategoryListSerializer(categories, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-@api_view(['GET'])
-def product_detail_view(request, id):
-    product = get_object_or_404(Product, id=id)
-    serializer = ProductDetailSerializer(product)
+    if request.method == 'DELETE':
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    serializer = CategoryDetailSerializer(category, data=request.data, partial=(request.method=='PATCH'))
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def product_list_view(request):
     products = Product.objects.all()
     serializer = ProductListSerializer(products, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-@api_view(['GET'])
-def review_detail_view(request, id):
-    review = get_object_or_404(Review, id=id)
-    serializer = ReviewDetailSerializer(review)
     return Response(serializer.data)
+@api_view(['GET'])
+def product_detail_view(request,id):
+    product=get_object_or_404(Product,id=id)
+    serializer=ProductDetailSerializer(product)
+    return Response(serializer.data)
+@api_view(['POST'])
+def product_create_view(request):
+    serializer = ProductDetailSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+@api_view(['PUT','PATCH','DELETE'])
+def product_modify_view(request,id):
+    product = get_object_or_404(Product,id=id)
+
+    if request.method =='DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    serializer = ProductDetailSerializer(product,data =request.data, partial=(request.method=='PATCH'))
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data)
+
 
 @api_view(['GET'])
 def review_list_view(request):
     reviews = Review.objects.all()
     serializer = ReviewListSerializer(reviews, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.data)
+@api_view(['GET'])
+def review_detail_view(request,id):
+    review=get_object_or_404(Review,id=id)
+    serializer=ReviewDetailSerializer(review)
+    return Response(serializer.data)
+@api_view(['POST'])
+def review_create_view(request):
+    serializer = ReviewDetailSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+@api_view(['PUT','PATCH','DELETE'])
+def review_modify_view(request,id):
+    review = get_object_or_404(Review,id=id)
+
+    if request.method =='DELETE':
+        review.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    serializer = ReviewDetailSerializer(review,data =request.data, partial=(request.method=='PATCH'))
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data)
--- shop_api/urls.py
+++ shop_api/urls.py
@@ -17,17 +17,26 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from product.views import (
-    category_list_view, category_detail_view,
-    product_list_view, product_detail_view,
-    review_list_view, review_detail_view
+    category_list_view, category_detail_view,category_create_view,category_modify_view,
+    product_list_view, product_detail_view,product_create_view,product_modify_view,
+    review_list_view, review_detail_view,review_create_view,review_modify_view
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/categories/', category_list_view),
-    path('api/v1/categories/<int:id>/', category_detail_view),
-    path('api/v1/products/', product_list_view),
-    path('api/v1/products/<int:id>/', product_detail_view),
-    path('api/v1/reviews/', review_list_view),
-    path('api/v1/reviews/<int:id>/', review_detail_view),
+    path('api/v1/categories/', category_list_view,name='category_list'),
+    path('api/v1/categories/create/', category_create_view, name='category_create'),
+    path('api/v1/categories/<int:id>/', category_detail_view, name='category_datail'),
+    path('api/v1/categories/<int:id>/modify/', category_modify_view, name='category_modify'),
+    path('api/v1/products/', product_list_view,name='product_list'),
+    path('api/v1/products/create/', product_create_view, name='product_create'),
+    path('api/v1/products/<int:id>/', product_detail_view, name='product_datail'),
+    path('api/v1/products/<int:id>/modify/', product_modify_view, name='product_modify'),
+    path('api/v1/reviews/', review_list_view,name='review_list'),
+    path('api/v1/reviews/create/', review_create_view, name='review_create'),
+    path('api/v1/reviews/<int:id>/', review_detail_view, name='review_datail'),
+    path('api/v1/reviews/<int:id>/modify/', review_modify_view, name='review_modify'),
+
+
 ]
+
